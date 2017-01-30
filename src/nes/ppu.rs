f85a1b773ab494cce515c217378a2f7b7cecf343
Walter Kuppens <reshurum@gmail.com>
2017-01-29 19:03:52 -0800

    Added some PPU register utility functions

--- src/nes/ppu.rs
+++ src/nes/ppu.rs
@@ -59,6 +59,40 @@ const INITIAL_PPUADDR:   u8 = 0b00000000;
 const INITIAL_PPUDATA:   u8 = 0b00000000;
 const INITIAL_OAMDMA:    u8 = 0b00000000;
 
+// Bitmask values for PPU registers.
+const PPUCTRL_BASE_NAMETABLE_ADDRESS:           u8 = 0b00000011;
+const PPUCTRL_VRAM_ADDRESS_INCREMENT:           u8 = 0b00000100;
+const PPUCTRL_SPRITE_PATTERN_TABLE_ADDRESS:     u8 = 0b00001000;
+const PPUCTRL_BACKGROUND_PATTERN_TABLE_ADDRESS: u8 = 0b00010000;
+const PPUCTRL_SPRITE_SIZE:                      u8 = 0b00100000;
+const PPUCTRL_MASTER_SLAVE_SELECT:              u8 = 0b01000000;
+const PPUCTRL_NMI_ENABLE:                       u8 = 0b10000000;
+const PPUMASK_GREYSCALE:                        u8 = 0b00000001;
+const PPUMASK_SHOW_BACKGROUND_LEFT:             u8 = 0b00000010;
+const PPUMASK_SHOW_SPRITES_LEFT:                u8 = 0b00000100;
+const PPUMASK_SHOW_BACKGROUND:                  u8 = 0b00001000;
+const PPUMASK_SHOW_SPRITES:                     u8 = 0b00010000;
+const PPUMASK_EMPHASIZE_RED:                    u8 = 0b00100000;
+const PPUMASK_EMPHASIZE_GREEN:                  u8 = 0b01000000;
+const PPUMASK_EMPHASIZE_BLUE:                   u8 = 0b10000000;
+const PPUSTATUS_REGISTER_BITS:                  u8 = 0b00011111;
+const PPUSTATUS_SPRITE_OVERFLOW:                u8 = 0b00100000;
+const PPUSTATUS_SPRITE_0_HIT:                   u8 = 0b01000000;
+const PPUSTATUS_VBLANK:                         u8 = 0b10000000;
+
+/// SpriteSize is used by flag reading functions when sprite size information is
+/// required at runtime.
+enum SpriteSize {
+    Bounds8x8,
+    Bounds8x16,
+}
+
+/// MasterSlaveSelect is used by the PPU master slave flag.
+enum MasterSlaveSelect {
+    ReadBackdrop,
+    OutputColor,
+}
+
 /// This is an implementation of the 2C02 PPU used in the NES. This piece of
 /// hardware is responsible for drawing graphics to the television the console
 /// is hooked up to; however in our case we draw to an SDL surface.
@@ -154,15 +188,152 @@ impl PPU {
     }
 
     /// Reads a byte from PPU memory at the given virtual address.
+    #[inline(always)]
     fn read_u8(&mut self, addr: usize) -> u8 {
         let (bank, addr) = self.map(addr);
         bank[addr]
     }
 
     /// Writes a byte to PPU memory at the given virtual address.
+    #[inline(always)]
     fn write_u8(&mut self, addr: usize, value: u8) {
         let (bank, addr) = self.map(addr);
         bank[addr] = value;
+    }
+
+    /// Returns the base nametable address currently set in PPUCTRL.
+    #[inline(always)]
+    fn ppu_ctrl_base_nametable_address(&self) -> usize {
+        match self.ppu_ctrl & PPUCTRL_BASE_NAMETABLE_ADDRESS {
+            0 => 0x2000,
+            1 => 0x2400,
+            2 => 0x2800,
+            3 => 0x2C00,
+            _ => panic!("Invalid nametable address index, physics is broken as 2-bit number is not 2-bit?"),
+        }
+    }
+
+    /// Returns the current VRAM increment value.
+    #[inline(always)]
+    fn ppu_ctrl_vram_address_increment(&self) -> u8 {
+        match self.ppu_ctrl & PPUCTRL_VRAM_ADDRESS_INCREMENT {
+            0 => 1,
+            _ => 32,
+        }
+    }
+
+    /// Returns the current sprite pattern table address.
+    #[inline(always)]
+    fn ppu_ctrl_sprite_pattern_table_address(&self) -> usize {
+        match self.ppu_ctrl & PPUCTRL_SPRITE_PATTERN_TABLE_ADDRESS {
+            0 => 0x0000,
+            _ => 0x1000,
+        }
+    }
+
+    /// Returns the current background pattern table address.
+    #[inline(always)]
+    fn ppu_ctrl_background_pattern_table_address(&self) -> usize {
+        match self.ppu_ctrl & PPUCTRL_BACKGROUND_PATTERN_TABLE_ADDRESS {
+            0 => 0x0000,
+            _ => 0x1000,
+        }
+    }
+
+    /// Returns the currently selected sprite sizes in use.
+    #[inline(always)]
+    fn ppu_ctrl_sprite_size(&self) -> SpriteSize {
+        match self.ppu_ctrl & PPUCTRL_SPRITE_SIZE {
+            0 => SpriteSize::Bounds8x8,
+            _ => SpriteSize::Bounds8x16,
+        }
+    }
+
+    /// Returns the PPU master slave select state.
+    #[inline(always)]
+    fn ppu_ctrl_master_slave_select(&self) -> MasterSlaveSelect {
+        match self.ppu_ctrl & PPUCTRL_MASTER_SLAVE_SELECT {
+            0 => MasterSlaveSelect::ReadBackdrop,
+            _ => MasterSlaveSelect::OutputColor,
+        }
+    }
+
+    /// Returns true if the NMI timer is currently enabled.
+    #[inline(always)]
+    fn ppu_ctrl_nmi_enabled(&self) -> bool {
+        self.ppu_ctrl & PPUCTRL_NMI_ENABLE > 0
+    }
+
+    /// Returns true if greyscale mode is enabled.
+    #[inline(always)]
+    fn ppu_mask_greyscale(&self) -> bool {
+        self.ppu_mask & PPUMASK_GREYSCALE > 0
+    }
+
+    /// Returns the state of the PPUMASK_SHOW_BACKGROUND_LEFT flag.
+    #[inline(always)]
+    fn ppu_mask_show_background_left(&self) -> bool {
+        self.ppu_mask & PPUMASK_SHOW_BACKGROUND_LEFT > 0
+    }
+
+    /// Returns the state of the PPUMASK_SHOW_SPRITES_LEFT flag.
+    #[inline(always)]
+    fn ppu_mask_show_sprites_left(&self) -> bool {
+        self.ppu_mask & PPUMASK_SHOW_SPRITES_LEFT > 0
+    }
+
+    /// Returns the state of the PPUMASK_SHOW_BACKGROUND flag.
+    #[inline(always)]
+    fn ppu_mask_show_background(&self) -> bool {
+        self.ppu_mask & PPUMASK_SHOW_BACKGROUND > 0
+    }
+
+    /// Returns the state of the PPUMASK_SHOW_SPRITES flag.
+    #[inline(always)]
+    fn ppu_mask_show_sprites(&self) -> bool {
+        self.ppu_mask & PPUMASK_SHOW_SPRITES > 0
+    }
+
+    /// Returns the state of the PPUMASK_EMPHASIZE_RED flag.
+    #[inline(always)]
+    fn ppu_mask_emphasize_red(&self) -> bool {
+        self.ppu_mask & PPUMASK_EMPHASIZE_RED > 0
+    }
+
+    /// Returns the state of the PPUMASK_EMPHASIZE_GREEN flag.
+    #[inline(always)]
+    fn ppu_mask_emphasize_green(&self) -> bool {
+        self.ppu_mask & PPUMASK_EMPHASIZE_GREEN > 0
+    }
+
+    /// Returns the state of the PPUMASK_EMPHASIZE_BLUE flag.
+    #[inline(always)]
+    fn ppu_mask_emphasize_blue(&self) -> bool {
+        self.ppu_mask & PPUMASK_EMPHASIZE_BLUE > 0
+    }
+
+    /// Returns the state of the PPUSTATUS_REGISTER_BITS flag.
+    #[inline(always)]
+    fn ppu_status_register_bits(&self) -> u8 {
+        self.ppu_status & PPUSTATUS_REGISTER_BITS
+    }
+
+    /// Returns the state of the PPUSTATUS_SPRITE_OVERFLOW flag.
+    #[inline(always)]
+    fn ppu_status_sprite_overflow(&self) -> bool {
+        self.ppu_status & PPUSTATUS_SPRITE_OVERFLOW > 0
+    }
+
+    /// Returns the state of the PPUSTATUS_SPRITE_0_HIT flag.
+    #[inline(always)]
+    fn ppu_status_sprite_0_hit(&self) -> bool {
+        self.ppu_status & PPUSTATUS_SPRITE_0_HIT > 0
+    }
+
+    /// Returns the state of the PPUSTATUS_VBLANK flag.
+    #[inline(always)]
+    fn ppu_status_vblank(&self) -> bool {
+        self.ppu_status & PPUSTATUS_VBLANK > 0
     }
 
     /// Copy data from main memory to the PPU's internal sprite memory.
@@ -194,6 +365,8 @@ impl PPU {
         }
         self.ppu_ctrl = memory.ppu_ctrl_registers[index];
         memory.ppu_ctrl_registers_status[index] = PPURegisterStatus::Untouched;
+
+        panic!("Implement PPUCTRL write handling");
     }
 
     /// Updates the internal PPUMASK register when the I/O register was written
@@ -206,6 +379,8 @@ impl PPU {
         }
         self.ppu_mask = memory.ppu_ctrl_registers[index];
         memory.ppu_ctrl_registers_status[index] = PPURegisterStatus::Untouched;
+
+        panic!("Implement PPUMASK write handling");
     }
 
     /// Updates the internal OAMADDR registers with data in the I/O register.
